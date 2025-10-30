b268d77c5c5b3fb63f54eee378c93793ee6f10b1
Bruna Pires <127413120+brunapires189@users.noreply.github.com>
2025-10-30 14:55:24 -0400

    Adicionado mais funcionalidades na atividade de git

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("meuBotao").addEventListener("click", function () {
+  var msgElemento = document.getElementById("mensagem");
+
+  msgElemento.innerText = "Trabalho finalizado com sucesso!";
+});
